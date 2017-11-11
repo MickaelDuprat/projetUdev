4a61ba19e966aa1b8cb564ac700693296efb77b1
MickaelDuprat <contact@mickaelduprat.fr>
2017-11-11 09:52:12 +0100

    Ajout de nouveaux dossiers au sein du projet et modifications mineures

---
+++ Site-web/include/header.php
@@ -0,0 +1,36 @@
+<!-- Header du site -->
+<header>
+	<h2>Vous êtes à la bonne adresse !</h2>
+	<img src="img/voiture.jpg" alt="Voiture"/>
+	<h3><i>Partons à la recherche de votre véhicule..</i></h3>
+	<div id="search">
+		<form class="form" id="form1">
+		  <br/>
+	      <i class="fa fa-calendar fa-2x" aria-hidden="true" "></i><input class="date feedback-input" type="text" placeholder="Date de départ" />
+
+	      <i class="fa fa-calendar fa-2x" aria-hidden="true"></i><input class="date feedback-input" type="text" placeholder="Date d'arrivée" />
+
+		  <div class="content">
+			<label for="toggle" class="admin-text"><div class="admin-panel">Types de véhicules</div></label>
+			<input type="checkbox" id="toggle">
+
+			<div class="menu">
+				<a href="#">Moto <img src="ico/motorcycle.png" alt="Moto"></a>
+				<a href="#">Citadine <img src="ico/car.png" alt="Citadine"></a>
+				<a href="#">Berline <img src="ico/car.png" alt="Berline"></a></a>
+				<a href="#">Utilitaire <img src="ico/van.png" alt="Utilitaire"></a></a>
+				<a href="#">SUV <img src="ico/car-2.png" alt="SUV"></a></span></a>
+				<a href="#">Luxe <img src="ico/car.png" alt="Luxe"></a></span></a>
+				<a href="#">Sport <img src="ico/car-1.png" alt="Sport"></a></span></a>
+			</div>
+		  </div>
+			
+		  <br/>
+		  
+	      <div class="submit">
+	        <input type="submit" value="Lancer la recherche" id="button-blue"/>
+	      </div>
+
+		</form>
+	</div>
+</header>
