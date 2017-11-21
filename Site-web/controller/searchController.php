1878b0e6cab1872f54a0cc47d18cda15cf91f846
bruce86100 <brucelostis@gmail.com>
2017-11-21 14:59:01 +0100

    a

--- Site-web/controller/searchController.php
+++ Site-web/controller/searchController.php
@@ -9,10 +9,11 @@ class SearchController{
     * private $variables
     *
     **/
+  private $manager;
 
-  // Constructeur qui initalise un objet manager instance de la classe AgenceModel 
+  // Constructeur qui initalise un objet manager instance de la classe SearchModel 
   public function __construct(){
-    $this->manager = new AgenceModel();
+    $this->manager = new SearchModel();
   }
 
   /** 
